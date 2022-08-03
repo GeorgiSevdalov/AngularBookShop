7c06bfce067cdd6616935286c78ca18af0434548
GeorgiSevdalov <nojegoje@gmail.com>
2022-08-03 16:18:34 +0300

    styled page using tailwindcss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BooksComponent } from './books/books.component';
-import { BookComponent } from './books/book/book.component';
 import { BooksModule } from "./books/books.module";
 import { CartComponent } from './cart/cart.component';
 
+
 @NgModule({
     declarations: [AppComponent, CartComponent],
-    imports: [BrowserModule, FormsModule, BooksModule],
+    imports: [BrowserModule, FormsModule, BooksModule, AppRoutingModule],
     bootstrap: [AppComponent]
 })
 
